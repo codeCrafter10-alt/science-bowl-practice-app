703d8a2eb4b10a908f68e52b7a35b5ba12188a24
codeCrafter10-alt <aaravg1410@gmail.com>
2026-05-15 16:19:15 -0700

    Add sample questions and basic gameplay

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,71 @@
 import { useState } from "react"
+import { sampleQuestions } from "./sampleQuestions";
 
 function App() {
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [answer, setAnswer] = useState("");
+  const [score, setScore] = useState(0);
+  const currentQuestion = sampleQuestions[questionIndex];
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const normalizedAnswer = answer.toLowerCase().trim();
+
+    const isCorrect = currentQuestion.answers.includes(normalizedAnswer);
+
+    if (isCorrect) {
+      setScore((prev) => prev + 4);
+    }
+
+    setAnswer("");
+    setQuestionIndex((prev) => prev + 1);
+  }
+
+  if (!currentQuestion) {
+    return(
+     <main>
+        <h1>End of Set</h1>
+
+        <p>
+          Final Score: {score}
+        </p>
+      </main>
+    );
+  }
+
   return (
     <main>
       <h1>Science Bowl App</h1>
+
+      <p>
+        Score: {score}
+      </p>
+
+      <p>
+        Question {questionIndex + 1} of{" "} {sampleQuestions.length}
+      </p>
+
+      <h2>
+        {currentQuestion.question}
+      </h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={answer}
+          onChange={(e) =>
+            setAnswer(e.target.value)
+          }
+          autoFocus
+        />
+
+        <button type="submit">
+          Submit
+        </button>
+      </form>
     </main>
-  )
+  );
 }
 
 export default App;
---
+++ src/sampleQuestions.js
@@ -0,0 +1,231 @@
+export const sampleQuestions = [
+  {
+    "id": 1,
+    "questionType": "tossup",
+    "linkedBonusId": 2,
+    "division": "high-school",
+    "type": "short-answer",
+    "topic": "Biology",
+    "question": "What is the most common term used in genetics to describe the observable physical characteristics of an organism caused by the expression of a gene or set of genes?",
+    "answers": ["phenotype"]
+  },
+  {
+    "id": 2,
+    "questionType": "bonus",
+    "division": "high-school",
+    "type": "short-answer",
+    "topic": "Biology",
+    "question": "What is the biological term most often used for the act of a cell engulfing a particle by extending its pseudopodia around the particle?",
+    "answers": ["phagocytosis"]
+  },
+
+  {
+    "id": 3,
+    "questionType": "tossup",
+    "linkedBonusId": 4,
+    "division": "high-school",
+    "type": "multiple-choice",
+    "topic": "Chemistry",
+    "question": "An aqueous solution in which the concentration of OH- ions is greater than that of H+ ions is:",
+    "choices": {
+      "W": "basic",
+      "X": "acidic",
+      "Y": "neutral",
+      "Z": "in equilibrium"
+    },
+    "answers": ["W", "basic"]
+  },
+  {
+    "id": 4,
+    "questionType": "bonus",
+    "division": "high-school",
+    "type": "short-answer",
+    "topic": "Chemistry",
+    "question": "Find the mass of 1 mole of cuprous oxide, or Cu2O. Assume the atomic mass of copper is 64 and oxygen is 16.",
+    "answers": ["144"]
+  },
+
+  {
+    "id": 5,
+    "questionType": "tossup",
+    "linkedBonusId": 6,
+    "division": "high-school",
+    "type": "short-answer",
+    "topic": "Physics",
+    "question": "What property of a sound wave is most commonly associated with loudness?",
+    "answers": ["amplitude"]
+  },
+  {
+    "id": 6,
+    "questionType": "bonus",
+    "division": "high-school",
+    "type": "short-answer",
+    "topic": "Physics",
+    "question": "What is the most common term for the behavior of light where it appears to bend around small obstacles or the spreading out of waves as light passes through pinholes or slits?",
+    "answers": ["diffraction"]
+  },
+
+  {
+    "id": 7,
+    "questionType": "tossup",
+    "linkedBonusId": 8,
+    "division": "high-school",
+    "type": "short-answer",
+    "topic": "Math",
+    "question": "What term best describes 2 angles with 90° as the sum of their measurements?",
+    "answers": ["complementary"]
+  },
+  {
+    "id": 8,
+    "questionType": "bonus",
+    "division": "high-school",
+    "type": "short-answer",
+    "topic": "Math",
+    "question": "Divide the following and give your answer in scientific notation: 8×10^7 / (4×10^4)^2",
+    "answers": ["4 × 10^5"]
+  },
+
+  {
+    "id": 9,
+    "questionType": "tossup",
+    "linkedBonusId": 10,
+    "division": "high-school",
+    "type": "multiple-choice",
+    "topic": "Earth Science",
+    "question": "The overall charge at the top and bottom, respectively, of a towering cumulonimbus cloud during a thunderstorm is:",
+    "choices": {
+      "W": "positive, positive",
+      "X": "positive, negative",
+      "Y": "negative, positive",
+      "Z": "negative, negative"
+    },
+    "answers": ["X", "positive, negative"]
+  },
+  {
+    "id": 10,
+    "questionType": "bonus",
+    "division": "high-school",
+    "type": "multiple-choice",
+    "topic": "Earth Science",
+    "question": "A lightning bolt is seen and its accompanying thunder is heard 15 seconds later. This means the storm is most likely how many miles away:",
+    "choices": {
+      "W": "3",
+      "X": "6",
+      "Y": "9",
+      "Z": "15"
+    },
+    "answers": ["W", "3"]
+  },
+
+  {
+    "id": 11,
+    "questionType": "tossup",
+    "linkedBonusId": 12,
+    "division": "high-school",
+    "type": "short-answer",
+    "topic": "General Science",
+    "question": "How many significant figures are in the number 0.00750?",
+    "answers": ["3"]
+  },
+  {
+    "id": 12,
+    "questionType": "bonus",
+    "division": "high-school",
+    "type": "short-answer",
+    "topic": "General Science",
+    "question": "Subtract the following 2 numbers and give your answer with the proper significant figures: 25.101 - 0.9608",
+    "answers": ["24.140"]
+  },
+
+  {
+    "id": 13,
+    "questionType": "tossup",
+    "linkedBonusId": 14,
+    "division": "high-school",
+    "type": "short-answer",
+    "topic": "Astronomy",
+    "question": "What is the proper name of the star that is most commonly noted to have coordinates closest to the north celestial pole?",
+    "answers": ["Polaris"]
+  },
+  {
+    "id": 14,
+    "questionType": "bonus",
+    "division": "high-school",
+    "type": "short-answer",
+    "topic": "Astronomy",
+    "question": "What is the most well-known asterism in Ursa Major?",
+    "answers": ["Big Dipper"]
+  },
+
+  {
+    "id": 15,
+    "questionType": "tossup",
+    "linkedBonusId": 16,
+    "division": "high-school",
+    "type": "multiple-choice",
+    "topic": "Biology",
+    "question": "Human epidermis is mostly composed of which of the following basic animal tissue types:",
+    "choices": {
+      "W": "epithelial",
+      "X": "connective",
+      "Y": "nervous",
+      "Z": "muscle"
+    },
+    "answers": ["W", "epithelial"]
+  },
+  {
+    "id": 16,
+    "questionType": "bonus",
+    "division": "high-school",
+    "type": "short-answer",
+    "topic": "Biology",
+    "question": "What are the 2 basic categories of lymphocyte cells?",
+    "answers": ["T cells", "B cells"]
+  },
+
+  {
+    "id": 17,
+    "questionType": "tossup",
+    "linkedBonusId": 18,
+    "division": "high-school",
+    "type": "short-answer",
+    "topic": "Chemistry",
+    "question": "What general type of bonding is found in molecules in which electrons are shared by nuclei?",
+    "answers": ["covalent"]
+  },
+  {
+    "id": 18,
+    "questionType": "bonus",
+    "division": "high-school",
+    "type": "short-answer",
+    "topic": "Chemistry",
+    "question": "Name all of the following 4 processes where latent heat is absorbed: evaporation; condensation; water melts; water freezes",
+    "answers": ["evaporation", "water melts"]
+  },
+
+  {
+    "id": 19,
+    "questionType": "tossup",
+    "linkedBonusId": 20,
+    "division": "high-school",
+    "type": "short-answer",
+    "topic": "Physics",
+    "question": "Although not known as such at the time, what was the first form of spectacular electric discharge seen by humans?",
+    "answers": ["lightning"]
+  },
+  {
+    "id": 20,
+    "questionType": "bonus",
+    "division": "high-school",
+    "type": "multiple-choice",
+    "topic": "Physics",
+    "question": "A constant force acting on a body experiencing no change in its environment will give the body:",
+    "choices": {
+      "W": "constant acceleration",
+      "X": "constant speed",
+      "Y": "constant velocity",
+      "Z": "zero acceleration"
+    },
+    "answers": ["W", "constant acceleration"]
+  }
+]
