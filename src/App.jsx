7d868cbb8a8c63e64751408bcbb191700528b674
codeCrafter10-alt <aaravg1410@gmail.com>
2026-05-25 20:04:03 -0700

    Add timer logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { sampleQuestions } from "./sampleQuestions";
 
 function App() {
@@ -8,9 +8,56 @@ function App() {
   const currentQuestion = sampleQuestions.find((question) => question.id === currentQuestionId);
   const [feedback, setFeedback] = useState("");
   const [showAnswer, setShowAnswer] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(5);
+
+  useEffect(() => {
+    if (currentQuestion?.questionType === "tossup") {
+      setTimeLeft(5);
+    } else {
+      setTimeLeft(20);
+    }
+  }, [currentQuestionId])
+
+  useEffect(() => {
+    if (timeLeft <= 0 || currentQuestionId === null) {
+      return;
+    }
+
+    const timer = setInterval(() => {
+      setTimeLeft((prev) => prev-1);
+    }, 1000)
+
+    return () => clearInterval(timer);
+  }, [timeLeft, currentQuestionId])
+
+  useEffect(() => {
+  if (timeLeft > 0 || feedback === "Correct!") {
+    return;
+  }
+
+  setFeedback("Time's up.");
+  setShowAnswer(true);
+  setAnswer("");
+
+  const timeout = setTimeout(() => {
+    setFeedback("");
+    setShowAnswer(false);
+    if (currentQuestion?.questionType === "bonus" || !currentQuestion?.linkedBonusId) {
+      goToNextQuestion(1);
+    } else {
+      goToNextQuestion(2);
+    }
+  }, 5000);
+
+  return () => clearTimeout(timeout);
+}, [timeLeft]);
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (timeLeft <= 0) {
+      return;
+    }
 
     const normalizedAnswer = answer.toLowerCase().trim();
 
@@ -60,7 +107,7 @@ function App() {
     }
   }
 
-  if (currentQuestion === null) {
+  if (!currentQuestion) {
     return(
      <main>
         <h1>End of Set</h1>
@@ -83,6 +130,7 @@ function App() {
       <p>
         Question {sampleQuestions.findIndex((question) => question.id === currentQuestionId) + 1} of {" "} {sampleQuestions.length}
       </p>
+      <h3 className="timer">Time Left: {timeLeft}</h3>
 
       <p className="division">
         {currentQuestion.division.split("-").map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(" ")}
